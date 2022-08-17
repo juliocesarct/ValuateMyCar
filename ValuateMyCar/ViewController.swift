0b476576855c4f2bc0fe11571ca0c62f4c4a6fd9
Júlio Teixeira <julioct@ciandt.com>
2022-08-17 11:15:48 -0300

    initial commit

---
+++ ValuateMyCar/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ValuateMyCar
+//
+//  Created by Júlio César Correia Teixeira on 03/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .gray
+    }
+
+
+}
+
